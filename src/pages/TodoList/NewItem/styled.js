2172a5077517699e86087d858135b6a44f9ff40e
Sergey Sivchenko <sergey.sivchenko@effective-soft.com>
2023-11-16 09:41:15 +0300

    feat(todo-list): add new item creation component

---
+++ src/pages/TodoList/NewItem/styled.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const AddButton = styled.button`
+  background: none;
+  border-radius: 6px;
+  border: none;
+  color: ${({ theme }) => theme.text.primary};
+  cursor: pointer;
+  font-size: 16px;
+  padding: 8px 16px;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  margin-top: 16px;
+`;
+
+export const Input = styled.input`
+  background: none;
+  border-radius: 6px;
+  border: 1px solid ${({ theme }) => theme.divider};
+  color: ${({ theme }) => theme.text.primary};
+  display: flex;
+  flex: 1;
+  font-size: 16px;
+  margin-right: 16px;
+  padding: 8px 16px;
+`;
